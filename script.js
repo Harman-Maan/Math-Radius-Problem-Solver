ed679966a56418f068d885823d9f89017c4877e2
maansaab95170 <maansaab95170@gmail.com>
2023-03-08 01:28:33 +0530

    New file

---
+++ script.js
@@ -0,0 +1,51 @@
+const radius = document.getElementById("Radius");
+const circle = document.getElementById("Circle");
+const semicircle = document.getElementById("Semicircle");
+const sphere = document.getElementById("Sphere");
+const hemisphere = document.getElementById("Hemisphere");
+const Thanks = document.getElementById("Thanks");
+
+let decimalPlaces = 2;
+
+function solve() {
+  // Collectng the values
+  const valueOfRadius = document.getElementById("radius-value").value;
+  const valueOfPie = document.querySelector('input[name="value-of-pie"]:checked').value;
+
+  // Assigning values to r and p so that it is easier to read formulas
+  let r = valueOfRadius;
+  let p;
+  if (valueOfPie == "22/7") p = 22 / 7;
+  else p = valueOfPie;
+
+  // Circle
+  let circleCurcumference = (2 * p * r).toFixed(decimalPlaces);
+  let circleArea = (p * r ** 2).toFixed(decimalPlaces);
+
+  // Semi-circle
+  let semicirclePerimeter = (p * r + 2 * r).toFixed(decimalPlaces);
+  let semicircleArea = ((p * r ** 2) / 2).toFixed(decimalPlaces);
+
+  // Sphere
+  let sphereVolume = ((4 / 3) * p * r ** 3).toFixed(decimalPlaces);
+  let sphereSurfaceArea = (4 * p * r ** 2).toFixed(decimalPlaces);
+
+  // Hemi-Sphere
+  let hemisphereVolume = ((2 / 3) * p * r ** 3).toFixed(decimalPlaces);
+  let hemisphereCurvedArea = (2 * p * r ** 2).toFixed(decimalPlaces);
+  let hemisphereTotalSurfaceArea = (3 * p * r ** 2).toFixed(decimalPlaces);
+
+  radius.innerHTML = "Radius is " + valueOfRadius + "<br>" + "(Value Pie is taken " + valueOfPie + ")";
+  circle.innerHTML = "<br><h3>Circle</h3>" + "<br>Circumference : " + circleCurcumference + "<br>Area : " + circleArea;
+  semicircle.innerHTML = "<br><h3>Semi-Circle</h3>" + "<br>Perimeter : " + semicirclePerimeter + "<br>Area : " + semicircleArea;
+  sphere.innerHTML = "<br><h3>Sphere</h3> " + "<br>Volume : " + sphereVolume + "<br>Surface Area : " + sphereSurfaceArea;
+  hemisphere.innerHTML =
+    "<br><h3>Hemi-Sphere</h3>" +
+    "<br>Volume : " +
+    hemisphereVolume +
+    "<br>Curved Surface Area : " +
+    hemisphereCurvedArea +
+    "<br>Total Surface Area : " +
+    hemisphereTotalSurfaceArea;
+  Thanks.innerHTML = "<br>Thanks for using";
+}
